4563e1aec884b64a7be45cdf449ee87fec1464f6
tecnofermoya <tecnofermoya@gmail.com>
2022-02-01 20:47:45 +0100

    Triángulo números terminado

--- triangulo_numeros.py
+++ triangulo_numeros.py
@@ -1,7 +1,6 @@
 altura = int(input('Altura del triángulo: '))
 
-for i in range(1, altura+1):
-    for j in range(1, i+1):
-        if j % 2 != 0:
-            print(j, end=' ')
+for i in range(1, (altura * 2) + 1, 2):
+    for j in range(i, 0, -2):
+        print(j, end=' ')
     print(' ')
